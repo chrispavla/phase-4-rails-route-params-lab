30d96534a766a85a022afe5c358505f5a53b3e24
Kristina <kristina.hodges7@gmail.com>
2022-09-20 11:16:52 -0400

    commit tree 96af220b820321b2c01715dae6d620f177ea6212

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # all the instances of a class
+    if params[:name]
+      students = Student.select{|student| student.first_name.downcase == params[:name] || student.last_name.downcase == params[:name]}
+    else
+      students = Student.all
+   end
     render json: students
   end
+  
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 
 end
